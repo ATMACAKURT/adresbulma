4438476766d4c71866a52486502131e7552594ff
Semih Ferik <feriksemih@gmail.com>
2021-11-09 11:40:51 +0300

    :wave: :wave: :wave: 25.10.2021 tarihli aktarilmis sql dosyası eklendi.

--- src/ptt-pk/fetch.py
+++ src/ptt-pk/fetch.py
@@ -10,7 +10,7 @@ connection = pymysql.connect(host='localhost',
 cursor = connection.cursor()
 
 try:
-    wb = load_workbook('data/pk_list_24.09.2021.xlsx')
+    wb = load_workbook('data/pk_list_25.10.2021.xlsx')
     ws = wb.active
 	
     lower_map = { ord(u'I'): u'ı' }
